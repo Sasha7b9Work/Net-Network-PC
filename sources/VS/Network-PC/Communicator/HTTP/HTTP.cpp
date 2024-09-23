6a8111d9e6dbba2c46d5714a8384103f87c6e42a
Sasha7b9Work <Phuntik-it@tut.by>
2024-09-23 10:01:45 +0300

    Скорректирован адрес для засылки в php-скрипт

--- sources/VS/Network-PC/Communicator/HTTP/HTTP.cpp
+++ sources/VS/Network-PC/Communicator/HTTP/HTTP.cpp
@@ -6,7 +6,7 @@
 
 namespace HTTP
 {
-    static const wxString url = "https://wltech.ru/wifi/post-data-mst.php";
+    static const wxString url = "https://wltech.ru/wifi/post-data-mst2.php";
     static const wxString content_type = "application/x-www-form-urlencoded"; 
     static const wxString key = "api_key=PtmAT51b3j4F8";
 }
